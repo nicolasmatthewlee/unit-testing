0ed909e0b8656c650f0ea7de3b5ad4194ac89440
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2022-12-12 17:04:45 -0800

    added testing for reverseString

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,10 @@
-import { capitalize } from './index.js';
+import test from 'node:test';
+import { capitalize, reverseString } from './index.js';
 
 test('capitalizes first letter of lowercase string', () => {
   expect(capitalize('george')).toBe('George');
+});
+
+test('reverses order of characters in a string', () => {
+  expect(reverseString('abcdefg')).toBe('gfedcba');
 });
